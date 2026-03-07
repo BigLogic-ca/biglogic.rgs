709ad32b6367f1828de6fbd55217db202a210264
Dario Passariello <dariopassariello@gmail.com>
2026-03-06 17:07:39 -0700

    Update tsup.plugin.types.ts

--- tsup.plugin.types.ts
+++ tsup.plugin.types.ts
@@ -3,45 +3,40 @@ import * as _path from 'node:path'
 
 import _pk from "./package.json"
 
-const Types = {
+const types = () => {
 
-  name: 'create-types',
-  buildEnd() {
-    // // Copy tools interface.d.ts files
-    // const
-    //   builtToolsDir = './dist/tools',
-    //   srcToolsDir = './tools'
+  const
+    builtToolsDir = './dist/tools',
+    srcToolsDir = './tools'
 
-    // if (fs.existsSync(srcToolsDir)) {
-    //   const tools = fs.readdirSync(srcToolsDir, { withFileTypes: true })
-    //   for (const t of tools) {
-    //     if (t.isDirectory()) {
-    //       const srcFile = path.join(srcToolsDir, t.name, 'interface.d.ts')
-    //       if (fs.existsSync(srcFile)) {
-    //         const destDir = path.join(builtToolsDir, t.name)
-    //         if (!fs.existsSync(destDir)) fs.mkdirSync(destDir, { recursive: true })
-    //         fs.copyFileSync(srcFile, path.join(destDir, 'interface.d.ts'))
-    //       }
-    //     }
-    //   }
-    // }
-
-    // // Patch dist/types/index.d.ts to reference [pk.code].d.ts
-    // // (tsc strips /// <reference> from .ts files when emitting .d.ts)
-    // const distIndexDts = './dist/index.d.ts'
-    // if (fs.existsSync(distIndexDts)) {
-    //   const content = fs.readFileSync(distIndexDts, 'utf8')
-    //   const ref = `/// <reference path="./${pk.code}.d.ts" />\n`
-    //   if (!content.startsWith(ref)) {
-    //     fs.writeFileSync(distIndexDts, ref + content, 'utf8')
-    //     console.debug(`-----> Patched dist/types/index.d.ts with ${pk.code}.d.ts reference.`)
-    //   }
-    // }
-
-    return
-
+  if (_fs.existsSync(srcToolsDir)) {
+    const tools = _fs.readdirSync(srcToolsDir, { withFileTypes: true })
+    for (const t of tools) {
+      if (t.isDirectory()) {
+        const srcFile = _path.join(srcToolsDir, t.name, 'interface.d.ts')
+        if (_fs.existsSync(srcFile)) {
+          const destDir = _path.join(builtToolsDir, t.name)
+          if (!_fs.existsSync(destDir)) _fs.mkdirSync(destDir, { recursive: true })
+          _fs.copyFileSync(srcFile, _path.join(destDir, 'interface.d.ts'))
+        }
+      }
+    }
   }
+
+  // Patch dist/types/index.d.ts to reference dphelper.d.ts
+  // (tsc strips /// <reference> from .ts files when emitting .d.ts)
+  const distIndexDts = './dist/index.d.ts'
+  if (_fs.existsSync(distIndexDts)) {
+    const content = _fs.readFileSync(distIndexDts, 'utf8')
+    const ref = '/// <reference path="./dphelper.d.ts" />\n'
+    if (!content.startsWith(ref)) {
+      _fs.writeFileSync(distIndexDts, ref + content, 'utf8')
+      console.debug('-----> Patched dist/types/index.d.ts with dphelper.d.ts reference.')
+    }
+  }
+
+  return
 
 }
 
-export default Types
+export default types
