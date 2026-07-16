f42896e92c929a25a3d342b33381eabc9f7df6cf
Dario Passariello <dariopassariello@gmail.com>
2026-07-16 00:10:03 -0600

    Update npmUpdate.js

--- .dev/node/npmUpdate.js
+++ .dev/node/npmUpdate.js
@@ -1,6 +1,12 @@
 const execSync = require("node:child_process").execSync
 
 execSync("cd ../.. && npx npm-check-updates -u && npm i", { stdio: [0, 1, 2] })
-execSync("cd ../.. && socket patch scan && socket optimize && npx update-browserslist-db@latest -D", { stdio: [0, 1, 2] })
+
+execSync("cd ../../extension/vscode && npx npm-check-updates -u && npm i", { stdio: [0, 1, 2] })
+
+execSync("cd ../../tests && npx npm-check-updates -u && npm i -f", { stdio: [0, 1, 2] })
+
+// execSync("cd ../.. && npm i -D browserslist caniuse-lite baseline-browser-mapping", { stdio: [0, 1, 2] })
+// execSync("cd ../.. && npx socket patch scan && npx socket optimize && npx update-browserslist-db@latest", { stdio: [0, 1, 2] })
 
 execSync("node changeVersion.js", { stdio: [0, 1, 2] })
