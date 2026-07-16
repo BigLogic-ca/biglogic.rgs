5a06073fb2a7b45bf16984870e7a15a083e26964
Dario Passariello <dariopassariello@gmail.com>
2026-07-16 00:11:07 -0600

    Update tsup.setup.ts

--- tsup.setup.ts
+++ tsup.setup.ts
@@ -1,8 +1,4 @@
 export const include = [
-  { from: '.github/COPYRIGHT.md', to: 'COPYRIGHT.md' },
-  { from: '.github/LICENSE.md', to: 'LICENSE.md' },
-  { from: '.github/SECURITY.md', to: 'SECURITY.md' },
-  { from: '.github/FUNDING.yml', to: 'FUNDING.yml' },
   { from: 'package.json', to: 'package.json' },
   { from: 'types/', to: 'types/' },
   { from: 'docs/', to: '.' }
